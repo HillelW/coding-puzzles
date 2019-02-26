dd8d6cc664e7ab5f4af5c2ea03fbead364582583
@hillelw <hillelwolin@gmail.com>
2019-02-26 17:09:58 -0500

    define ListNode and AddwoNumbers

---
+++ src/linkedList/ListNode.java
@@ -0,0 +1,13 @@
+package linkedList;
+
+/**
+ * Definition for singly-linked list.
+ */
+public class ListNode {
+	//state
+	int val;
+	ListNode next;
+	
+	//behavior
+	ListNode(int x) { val = x; }
+}
